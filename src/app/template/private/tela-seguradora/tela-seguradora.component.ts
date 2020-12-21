f6884739d909f3ae7524e25ca54eea9e62c952f8
felipe.custodio <felipeflzardo@gmail.com>
2020-12-21 12:50:17 -0300

    commit

--- src/app/template/private/tela-seguradora/tela-seguradora.component.ts
+++ src/app/template/private/tela-seguradora/tela-seguradora.component.ts
@@ -8,32 +8,25 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./tela-seguradora.component.scss']
 })
 export class TelaSeguradoraComponent implements OnInit {
+  showClient = false;
+  showProposta = false;
+  displayedColumns: string[] = ['position', 'name', 'documento', 'contato'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumnsProposta: string[] = ['position', 'name', 'documento', 'contato'];
+  dataSourceProposta = new MatTableDataSource<Proposta>(ELEMENT_DATA_PROPOSTA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   ngOnInit(): void {
 
   }
 
-  showClient: boolean = false;
-  showProposta: boolean = false;
-
-  displayedColumns: string[] = ['position', 'name', 'documento', 'contato'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
-
-  displayedColumnsProposta: string[] = ['position', 'name', 'documento', 'contato'];
-  dataSourceProposta = new MatTableDataSource<Proposta>(ELEMENT_DATA_PROPOSTA);
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-
-
-
-
-  showClientModel() {
+  showClientModel(): void {
     if (this.showProposta === true) {
       this.showProposta = !this.showProposta;
     }
     this.showClient = !this.showClient;
   }
+
   showPropostaModel() {
     if (this.showClient === true) {
       this.showClient = !this.showClient;
@@ -41,7 +34,7 @@ export class TelaSeguradoraComponent implements OnInit {
     this.showProposta = !this.showProposta;
   }
 
-  // troca de pagina 
+  // troca de pagina
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
@@ -55,6 +48,7 @@ export interface PeriodicElement {
   contato: string;
 
 }
+
 export interface Proposta {
   name: string;
   position: number;
@@ -65,22 +59,22 @@ export interface Proposta {
 
 // elementos de da tabela
 const ELEMENT_DATA_PROPOSTA: Proposta[] = [
-  { position: 1, name: 'Hydrogen', documento: '600', contato: '(00) 0000-0000', },
-  { position: 2, name: 'Helium', documento: '300', contato: '(00) 0000-0000', },
-  { position: 3, name: 'Lithium', documento: '250', contato: '(00) 0000-0000', },
-  { position: 4, name: 'Beryllium', documento: '900', contato: '(00) 0000-0000', },
-  { position: 5, name: 'Boron', documento: '800', contato: '(00) 0000-0000', },
-  { position: 6, name: 'Carbon', documento: '500', contato: '(00) 0000-0000', },
-  { position: 7, name: 'Nitrogen', documento: '750', contato: '(00) 0000-0000', },
+  {position: 1, name: 'Hydrogen', documento: '600', contato: '(00) 0000-0000',},
+  {position: 2, name: 'Helium', documento: '300', contato: '(00) 0000-0000',},
+  {position: 3, name: 'Lithium', documento: '250', contato: '(00) 0000-0000',},
+  {position: 4, name: 'Beryllium', documento: '900', contato: '(00) 0000-0000',},
+  {position: 5, name: 'Boron', documento: '800', contato: '(00) 0000-0000',},
+  {position: 6, name: 'Carbon', documento: '500', contato: '(00) 0000-0000',},
+  {position: 7, name: 'Nitrogen', documento: '750', contato: '(00) 0000-0000',},
 ];
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', documento: '755.707.730-02', contato: '(00) 0000-0000', },
-  { position: 2, name: 'Helium', documento: '755.707.730-02', contato: '(00) 0000-0000', },
-  { position: 3, name: 'Lithium', documento: '755.707.730-02', contato: '(00) 0000-0000', },
-  { position: 4, name: 'Beryllium', documento: '755.707.730-02', contato: '(00) 0000-0000', },
-  { position: 5, name: 'Boron', documento: '755.707.730-02', contato: '(00) 0000-0000', },
-  { position: 6, name: 'Carbon', documento: '755.707.730-02', contato: '(00) 0000-0000', },
-  { position: 7, name: 'Nitrogen', documento: '755.707.730-02', contato: '(00) 0000-0000', },
+  {position: 1, name: 'Hydrogen', documento: '755.707.730-02', contato: '(00) 0000-0000',},
+  {position: 2, name: 'Helium', documento: '755.707.730-02', contato: '(00) 0000-0000',},
+  {position: 3, name: 'Lithium', documento: '755.707.730-02', contato: '(00) 0000-0000',},
+  {position: 4, name: 'Beryllium', documento: '755.707.730-02', contato: '(00) 0000-0000',},
+  {position: 5, name: 'Boron', documento: '755.707.730-02', contato: '(00) 0000-0000',},
+  {position: 6, name: 'Carbon', documento: '755.707.730-02', contato: '(00) 0000-0000',},
+  {position: 7, name: 'Nitrogen', documento: '755.707.730-02', contato: '(00) 0000-0000',},
 ];
 
 
